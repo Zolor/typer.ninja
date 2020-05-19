bc9bfcfa7ca83fb43b144e1266a37e0ad74da4dc
Erika Forslöf <erikaforslov@hotmail.com>
2020-05-19 22:30:59 +0200

    More word checkers
    Not 100% working but I only had so much time today

--- static/typer.js
+++ static/typer.js
@@ -4,11 +4,12 @@ let word_count = 0;
 function myFunction(event) {
   var x = event.which || event.keyCode;
   var textProperty = 'textContent' in document ? 'textContent' : 'innerText';
-  if (x == 32) {
+  console.log(document.getElementById('typer').value.length)
+  if (x == 32 && document.getElementById('typer').value.length > 0) {
     let next = word_count + 1;
     document.querySelector(`[word="${next}"]`).classList.add('highlight');
     document.querySelector(`[word="${word_count}"]`).classList.remove('highlight');
-
+    /* Highlight previous word according if it's correctly spelled or not */
     if (document.getElementById('typer').value.trim() == document.querySelector(`[word="${word_count}"]`).innerText) {
         document.querySelector(`[word="${word_count}"]`).classList.add('correct_highlight');
     }
@@ -16,6 +17,23 @@ function myFunction(event) {
         document.querySelector(`[word="${word_count}"]`).classList.add('wrong_highlight');
     }
     word_count += 1;
-    document.getElementById('typer').value = '';
+    document.getElementById('typer').value = "";
+  }
+  /* if user makes a space without written any characters -> reset the input field */
+  else if (x == 32 && document.getElementById('typer').value.trim() === "") {
+    document.getElementById('typer').value = "";
+  }
+  /* make a check for every character typed if it's correctly spelled and act accordingly */
+  else {
+    let str = document.querySelector(`[word="${word_count}"]`).innerText;
+    str = str.slice(0, document.getElementById('typer').value.trim().length);
+    if (str != document.getElementById('typer').value.trim()) {
+      document.querySelector(`[word="${word_count}"]`).classList.add('wrong_highlight');
+    }
+    else {
+      document.querySelector(`[word="${word_count}"]`).classList.remove('wrong_highlight');
+    }
   }
 }
+
+/* Need to figure out why clearing the field leaves an empty space */
