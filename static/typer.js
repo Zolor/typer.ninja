a4be75571d7a94573619687c5c710769ac67ab0b
Erika Forslöf <erikaforslov@hotmail.com>
2020-05-20 20:25:13 +0200

    bug fix day
    - Fixed bug where every event left a space in input field
    - Started a timer field and got lost in CSS forrest

--- static/typer.js
+++ static/typer.js
@@ -1,26 +1,26 @@
 /* In this example, we use a cross-browser solution, because the keyCode property does not work on the onkeypress event in Firefox. However, the which property does.
 Explanation of the first line in the function below: if the browser supports event.which, then use event.which, otherwise use event.keyCode */
 let word_count = 0;
-function myFunction(event) {
-  var x = event.which || event.keyCode;
-  var textProperty = 'textContent' in document ? 'textContent' : 'innerText';
-  console.log(document.getElementById('typer').value.length)
-  if (x == 32 && document.getElementById('typer').value.length > 0) {
+let correct_counter = 0;
+function myGame(value) {
+  var x = value.slice(-1);
+  if (x == " " && document.getElementById('typer').value.length > 0) {
     let next = word_count + 1;
     document.querySelector(`[word="${next}"]`).classList.add('highlight');
     document.querySelector(`[word="${word_count}"]`).classList.remove('highlight');
     /* Highlight previous word according if it's correctly spelled or not */
     if (document.getElementById('typer').value.trim() == document.querySelector(`[word="${word_count}"]`).innerText) {
         document.querySelector(`[word="${word_count}"]`).classList.add('correct_highlight');
+        correct_counter ++;
     }
     else {
         document.querySelector(`[word="${word_count}"]`).classList.add('wrong_highlight');
     }
-    word_count += 1;
+    word_count ++;
     document.getElementById('typer').value = "";
   }
   /* if user makes a space without written any characters -> reset the input field */
-  else if (x == 32 && document.getElementById('typer').value.trim() === "") {
+  else if (x == " " && document.getElementById('typer').value.trim() === "") {
     document.getElementById('typer').value = "";
   }
   /* make a check for every character typed if it's correctly spelled and act accordingly */
@@ -35,5 +35,3 @@ function myFunction(event) {
     }
   }
 }
-
-/* Need to figure out why clearing the field leaves an empty space */
