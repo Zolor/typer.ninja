955bc8fd177b63bbea526b2b65c287cd9802b36e
Erika Forslöf <erikaforslov@hotmail.com>
2020-05-23 20:24:46 +0200

    Wordlist Addition
    Implemented wordlist and the ability to randomly generate wordlist via flask

--- main.py
+++ main.py
@@ -12,15 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import random
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
+# Read in word list into memory
+f = open("wordlist.txt", "r")
+word_list = []
+for i in f.readlines():
+    word_list.append(i.rstrip())
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html')
+
+@app.route('/wordlist')
+def wordlist():
+    # Randomize words from wordlist into a dict and return jsonify
+    item = 0
+    word_dict = {}
+    while item < 350:
+        word_dict[item] = random.choice(word_list)
+        item += 1
+    return jsonify(word_dict)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
